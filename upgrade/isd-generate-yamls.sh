4b3833f5a25ea1c977d839a7d0693899122ef2f1
saitejaopsmx <93385259+saitejaopsmx@users.noreply.github.com>
2023-03-20 17:49:28 +0530

    Updated the correct parameter

--- upgrade/isd-generate-yamls.sh
+++ upgrade/isd-generate-yamls.sh
@@ -5,14 +5,14 @@ cd /repo/
 ls -ltr
 sleep 30
 helm repo add staging-helm https://opsmx.jfrog.io/artifactory/opsmx-helm-local
-isdversion=$(helm search repo staging-helm/oes --versions | awk '{print $2,$3}' | grep "${version}" | head -1 | awk -F ' ' '{print $2}')
+isdversion=$(helm search repo staging-helm/oes --versions | awk '{print $2,$3}' | grep "${chartVersion}" | head -1 | awk -F ' ' '{print $2}')
 echo $isdversion
 beta=$(echo $isdversion | awk -F . '{print $NF}' | grep -c b)
 if [ "$beta" = "1" ]; then
   helm repo list
   helm repo update
   helm search repo staging-helm --versions
-  chartversion=$(helm search repo staging-helm/oes --versions | awk '{print $2,$3}' | grep "${version}" | head -1 | awk -F ' ' '{print $1}')
+  chartversion=$(helm search repo staging-helm/oes --versions | awk '{print $2,$3}' | grep "${chartVersion}" | head -1 | awk -F ' ' '{print $1}')
   helm pull staging-helm/oes --version="$chartversion"
 else
 helm repo add isd https://helmcharts.opsmx.com/
